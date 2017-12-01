1af132ad1a3ca6cef158134da1a5af57bc4650b8
bobby123jack <george.philbrick@mymail.champlain.edu>
2017-12-01 12:10:25 -0500

    updated radar basemap using google maps

--- WeatherParser.py
+++ WeatherParser.py
@@ -95,8 +95,17 @@ def weatherHourly( city, state, weather, hours):
     return
 
 def weatherRadar(city, state):
-    uri = 'http://api.wunderground.com/api/d1fec7897078849e/animatedradar/q/' + state + '/' + city + '.gif?num=15&timelabel=1&timelabel.x=30&timelabel.y=30&newmaps=1&width=750&height=750'
+    key = 'AIzaSyCyQOjgzcgd3zF--Fha9LD0oz5C_O71Fg8'
+    
+    uri = 'http://api.wunderground.com/api/d1fec7897078849e/animatedradar/q/' + state + '/' + city + '.gif?num=15&timelabel=1&timelabel.x=30&timelabel.y=30&newmaps=1&width=640&height=640'
     urllib.request.urlretrieve(uri, 'radar.gif')
+
+    
+    style = '&maptype=roadmap&style=element:geometry%7Ccolor:0x212121&style=element:labels%7Cvisibility:off&style=element:labels.icon%7Cvisibility:off&style=element:labels.text.fill%7Ccolor:0x757575&style=element:labels.text.stroke%7Ccolor:0x212121&style=feature:administrative%7Celement:geometry%7Ccolor:0x757575&style=feature:administrative.country%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:administrative.land_parcel%7Cvisibility:off&style=feature:administrative.locality%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.park%7Celement:geometry%7Ccolor:0x181818&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:poi.park%7Celement:labels.text.stroke%7Ccolor:0x1b1b1b&style=feature:road%7Celement:geometry.fill%7Ccolor:0x4e4e4e&style=feature:road%7Celement:labels.text.fill%7Ccolor:0x8a8a8a&style=feature:road.arterial%7Celement:geometry%7Ccolor:0x373737&style=feature:road.arterial%7Celement:geometry.fill%7Ccolor:0x9d9d9d&style=feature:road.highway%7Celement:geometry%7Ccolor:0x959595&style=feature:road.highway.controlled_access%7Celement:geometry%7Ccolor:0x4e4e4e&style=feature:road.local%7Celement:geometry.fill%7Ccolor:0x818181&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:transit%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:water%7Celement:geometry%7Ccolor:0x828282&style=feature:water%7Celement:labels%7Cvisibility:off&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x3d3d3d'
+    basemapuri = 'https://maps.googleapis.com/maps/api/staticmap?center=' + city + ',' + state + '&zoom=8&size=750x750' + style + '&key=AIzaSyCyQOjgzcgd3zF--Fha9LD0oz5C_O71Fg8'
+    urllib.request.urlretrieve(basemapuri, 'basemap.png')
+    
+weatherRadar('Burlington', 'VT')
 
     
     
